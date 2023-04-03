02907186111b6e6a038115d212454b8a513456f9
Jordi Hoven <35233121+jordihoven@users.noreply.github.com>
2023-04-03 15:05:00 +0200

    Add ChatGPT improvements

--- src/app/translate.pipe.ts
+++ src/app/translate.pipe.ts
@@ -1,24 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { TranslateService } from './translate.service';
-
 // import { Translations } from '../enums/enums';
+
 @Pipe({
   name: 'translate',
-  pure: false
+  pure: false //pipe is called for every change detection cycle no matter whether the value or parameter(s) changes...
 })
 export class TranslatePipe implements PipeTransform {
   constructor(private translate: TranslateService) {}
 
-  transform(key: string): any {
-    const keys = key.split('.');
+  transform(key: string, delimiter: string = '.'): string {
+    const keys = key.split(delimiter);
     let value = this.translate.data;
     for (let i = 0; i < keys.length; i++) {
       value = value[keys[i]];
       if (!value) {
-        console.error("Non translated string");
-        break;
+        console.warn(`Translation not found for key: ${key}`);
+        return key;
       }
     }
-    return value || key.toString();
+    return value || key;
   }
 }
--- src/app/translate.service.ts
+++ src/app/translate.service.ts
@@ -2,29 +2,36 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-
 // import { Translations } from '../enums/enums';
 
 @Injectable({ 
   providedIn: 'root'
 })
 export class TranslateService {
-  data: any = {};
+  data: any = {}; //create an empty object...
   constructor(private http: HttpClient) {}
+
   use(lang: string): Observable<any> {
-    const langPath = `assets/i18n/${lang || 'en'}.json`;
+    const validLangs = ['en', 'nl']; // Array of valid language codes
+    const langCode = validLangs.includes(lang) ? lang : 'en'; // Validate the lang parameter or use 'en' as default
+    const langPath = `assets/i18n/${langCode}.json`;
+
+    // Use Angular's HttpClient to make an HTTP GET request to the language file
     return this.http.get(langPath).pipe(
       map(response => {
+        // If the request is successful, set the response data to the `data` property of the TranslateService instance
         this.data = response || {};
+        // Return the json data for the current lang
         return this.data;
       }),
       catchError(() => {
+        // Return an empty object if there's an error (with the http request)
         this.data = {};
         return this.data;
       })
     );
   }
-  translate(key: string): string {
-    return this.data[key] || key.toString();
+  translate(key: string): string { //note: this method is used indirectly, removing it will not change the functionallity, but keeping it is safer...
+    return this.data[key] || key.toString(); //returns the value from the json, or show the string (if there's no translation)
   }
 }
